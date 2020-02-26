42c3d76df7bc8ff41d48818f0114c3d078dd467a
Luis <luisgomezruiz@gmail.com>
2020-02-26 17:16:44 +0100

    New Graph

--- MiniTutorials2/Assets/4-Graphs/Scripts/Graph.cs
+++ MiniTutorials2/Assets/4-Graphs/Scripts/Graph.cs
@@ -9,23 +9,32 @@ public class Graph : MonoBehaviour
 	[Range(10,100)]
 	public int resolution=10;
 
-	Transform[] points; 
-	
+	public GraphFunctionName function;
+
+	Transform[] points;
+
+	static GraphFunction[] functions= {SineFunction, MultiSineFunction };
 
 	private void Awake()
 	{
-		points = new Transform[resolution];
+		points = new Transform[resolution*resolution];
 		float step = 2f / resolution;
 
 		Vector3 scale = Vector3.one * step;
 		Vector3 position= new Vector3(0,0,0);
 
-		for (int i = 0; i < points.Length; i++)
+		for (int i = 0,x = 0, z = 0; i < points.Length; i++,x++)
 		{
-			
+			if (x==resolution)
+			{
+				x = 0;
+				z += 1;
+			}	
+
 			Transform point = Instantiate(pointPrefab);
 			points[i] = point;
-			position.x= ((i + 0.5f) * step - 1f);
+			position.x= (x + 0.5f) * step - 1f;
+			position.z = (z + 0.5f) * step - 1f;
 			point.localPosition = position;
 			point.localScale = scale;
 			point.SetParent(transform,false);
@@ -34,13 +43,28 @@ public class Graph : MonoBehaviour
 	}
 	private void Update()
 	{
+		float t = Time.time;
+		GraphFunction f=functions[(int)function];
+		
 		for (int i = 0; i < points.Length; i++)
 		{
 			Transform point = points[i];
 			Vector3 position = point.localPosition;
-			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+			position.y = f(position.x,position.z, t);
 			point.localPosition = position;
 		}
 
+	}
+	static float SineFunction(float x,float z, float t)
+	{
+		return Mathf.Sin(Mathf.PI*(x+t));	 
+	}
+
+	static float MultiSineFunction(float x,float z, float t)
+	{
+		float y= Mathf.Sin(Mathf.PI * (x + t));
+		y += Mathf.Sin(2f*Mathf.PI*(x+2f*t))/2f;
+		y *= 2f / 3f;
+		return y;
 	}
 }
---
+++ MiniTutorials2/Assets/4-Graphs/Scripts/GraphFunction.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public delegate float GraphFunction(float x,float z, float t);
+
